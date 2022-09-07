cf96e265c89d7ed565bdcdded1f3e3d5c1c3d0e9
zickbalazs <zickbalazs@turr.hu>
2022-09-07 14:05:10 +0200

    2022.09.07. napi update

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express'),
+      path = require('path'),
       app = express();
+path.dirname(path.join(__dirname, './'))
+//CSS Mappa feldolgozása
+app.use('/css', express.static(path.join(__dirname, './css')));
+app.use(express.urlencoded({extended:true}))
+
+
 app.get('/', (req, res)=>{
-    res.status(200).send('AAAA')
+    res.status(200).sendFile(path.join(__dirname,"./index.html"));
+});
+app.get('/users', (req, res)=>{
+    res.status(200).sendFile(path.join(__dirname,"./users.html"));
+});
+app.get('/contacts', (req, res)=>{
+    res.status(200).sendFile(path.join(__dirname,"contacts.html"));
+});
+app.post('/api/message', (req,res)=>{
+    console.log(req.body);
+    res.status(200).sendFile(path.join(__dirname,"./thx.html"));
 })
-app.listen(3000, console.log('http://localhost:3000'))
+app.listen(3000, console.log('http://localhost:3000'));
